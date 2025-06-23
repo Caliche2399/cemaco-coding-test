359a7316a3bf0ecfea48dbf09afebcc561299fca
Carlos Aguilar <carlosaguilarpaniagua@gmail.com>
2025-06-23 08:47:37 -0600

    Agregue react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import {FooterComponent} from "./components/footer-component.tsx";
 import NavbarComponent from "./components/navbar-component.tsx";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {Container} from "react-bootstrap";
 
 function App() {
 
   return (
-    <>
+    <Router>
       <NavbarComponent />
+      <div id="content">
+        <Container>
+          <Routes>
+            <Route path="/" element={ <></>} />
+            <Route path="/login" element={<></>} />
+            <Route path="/products" element={<></>} />
+          </Routes>
+        </Container>
+      </div>
       <FooterComponent />
-    </>
+    </Router>
   )
 }
 
